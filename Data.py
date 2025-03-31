4731ba2ed473a4a884dc664179eb18b0db77e7b3
Hamza Qureshi <hamzaq2400@gmail.com>
2025-03-31 11:43:33 +0100

    Data Cleaning
    Cleaning the data using dropna and drop_duplicates to remove any inconsistencies.

--- Data.py
+++ Data.py
@@ -9,3 +9,17 @@ df_full = pd.read_csv("Trips_Full_Data.csv")
 ## Loading Datasets using Dask
 df_distance_dask = dd.read_csv("Trips_By_Distance.csv")
 df_full_dask = dd.read_csv("Trips_Full_Data.csv")
+
+## Clean the data using dropna and drop_duplicates to remove inconsistencies 
+df_distance.dropna(inplace = True)
+df_distance.drop_duplicates(inplace = True)
+
+df_full.dropna(inplace = True)
+df_full.drop_duplicates(inplace = True)
+
+## Do the same for the datasets loaded using dask
+df_distance_dask.dropna(inplace = True)
+df_distance_dask.drop_duplicates(inplace = True)
+
+df_full_dask.dropna(inplace = True)
+df_full_dask.drop_duplicates(inplace = True)
